c3c85a75a4ae832ee9abe567a0672e0b0f02da3c
jagatheesan2345 <theesun6195@gmail.com>
2023-01-13 05:04:15 +0530

    kj

--- src/org/assus/Insert.java
+++ src/org/assus/Insert.java
@@ -2,6 +2,10 @@ package org.assus;
 
 public class Insert {
 
+	public void kamal() {
+		
+System.out.println("kamal love kamali");
+	}
 	public static void main(String[] args) {
 		
 System.out.println("ahi");
